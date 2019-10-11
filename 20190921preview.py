ba96907d94ae1d1d3157e72057c09f6a25877810
Sunky <lixiaomin0163@163.com>
2019-10-11 13:53:12 +0800

    提交20190921的预习作业

--- 20190921preview.py
+++ 20190921preview.py
@@ -1,12 +1,18 @@
 __author__ = 'Sunky'
 #  -*-coding:utf8 -*-
 #3.求10的阶乘
-# def factorial(n):
-#     res=1
-#     for i in  range(1,n+1):
-#         res *= i
-#         return res
-#         print(i)
+# num = int(input("请输入一个数字: "))
+# factorial = 1
+#
+# # 判断阶乘
+# if num < 0:
+#    print("抱歉，负数没有阶乘")
+# elif num == 0:
+#    print("0 的阶乘为 1")
+# else:
+#    for i in range(1,num + 1):
+#        factorial = factorial*i
+#    print("%d 的阶乘为 %d" %(num,factorial))
 
 #4.求100以内能被3整除的数，并将作为列表输出
 # num =1
@@ -21,10 +27,19 @@ __author__ = 'Sunky'
 # print(formatList)
 
 #6.求斐波那契数列 1 2 3 5 8 13 ……
+# def fib_loop_while(max):
+#     a, b = 0, 1
+#     while max > 0:
+#         a, b = b, a + b
+#         max -= 1
+#         yield a
+#
+# for i in fib_loop_while(10):
+#     print(i)
 
 #7.求10000以内的质数
-primeNum =2
-for i in range(10000):
-    if i % 2 !=0:
-        print(i)
-        primeNum +=1
+# primeNum =2
+# for i in range(10000):
+#     if i % 2 !=0:
+#         print(i)
+#         primeNum +=1
