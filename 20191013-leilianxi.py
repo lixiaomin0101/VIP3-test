e84e756692fe6fec569b23aa305f49e12650139f
Sunky <lixiaomin0163@163.com>
2019-10-14 19:10:38 +0800

    1013课堂练习题

---
+++ 20191013-leilianxi.py
@@ -0,0 +1,38 @@
+__author__ = 'Sunky'
+#  -*-coding:utf8 -*-
+
+#定义一个学生类：Student、内部含有一个方法：study，实现打印：xx学习xx课程
+# class Student():
+#     def study(self,name,course):
+#         print(name+'学习'+ course + '课程')
+# a =Student()
+# a.study('张三','语文')
+
+
+#定义一个类名：Student—学生、类内部含有一个属性：sno—学号，一个方法：study—学习，
+# 实现打印：学号为xx的学生，学习xx课程
+# class Student():
+#     def __init__(self,sno,course):
+#         self.sno=sno
+#         self.course=course
+#
+#     def study(self):
+#         print(self.sno+'学号的学生，学习'+self.course+'课程')
+# a = Student('001','数学')
+# a.study()
+
+
+#定义一个Teacher类，继承Person类，拥有自身的属性工号：gh，自身的方法：teach教课（课程）；
+# 1、实现gh为xx的老师，教xx课
+# 2、实现gh为xx老师，在xx上班，一月工资xx
+# 3、名字是xx，工号为xx的老师，吃饭
+
+class Person():
+    def __init__(self,name,sex,age):
+        self.name=name
+        self.sex=sex
+        self.age=age
+    def teach(self,gh,kecheng,):
+        print(self.name+'老师'+self.sex+self.age+'岁'+)
+class Teacher(Person):
+
